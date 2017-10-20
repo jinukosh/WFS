7064c05106692c5bad61e4d21a272a5c183b6bec
Jinu Koshy <jinukosh@gmail.com>
2017-10-20 22:58:58 +1100

    Read me file

---
+++ README.md
@@ -0,0 +1,10 @@
+Please follow the below steps to deploy and run the application
+
+Publish Database Project
+Set database connection details and create database.
+Change configuration files of Db project and Web API project.
+Publish the SPA and WebAPI projects to IIS
+
+Use the below credentials to login
+jinu@admin.com
+Password: 123
--- WFS.Database/dbo/Values/Script.PostDeployment.sql
+++ WFS.Database/dbo/Values/Script.PostDeployment.sql
@@ -13,16 +13,14 @@ INSERT [dbo].[CommonUserType] ([Id], [Name], [IsDeleted]) VALUES (1, N'Admin', 0
 GO
 INSERT [dbo].[CommonUserType] ([Id], [Name], [IsDeleted]) VALUES (2, N'User', 0)
 GO
-INSERT [dbo].[CommonUserType] ([Id], [Name], [IsDeleted]) VALUES (3, N'Client', 0)
-GO
 SET IDENTITY_INSERT [dbo].[CommonUserType] OFF
 GO
 
 SET IDENTITY_INSERT [dbo].[User] ON 
 GO
-INSERT [dbo].[User] ([Id], [Name], [Email], [Password], [UserType], [CreatedDate], [CreatedIpAddress], [CreatedUserId], [UpdatedDate], [UpdatedIpAddress], [UpdatedUserId], [IsDeleted]) VALUES (1, N'Adam Smith', N'admin@admin.com', '1', 1, CAST(N'2015-01-01 00:00:00.000' AS DateTime), N'192.168.1.1', NULL, NULL, N'192.168.1.1', NULL, 0)
+INSERT [dbo].[User] ([Id], [Name], [Email], [Password], [UserType], [CreatedDate], [CreatedIpAddress], [CreatedUserId], [UpdatedDate], [UpdatedIpAddress], [UpdatedUserId], [IsDeleted]) VALUES (1, N'Jinu Koshy', N'jinu@admin.com', '123', 1, CAST(N'2015-01-01 00:00:00.000' AS DateTime), N'192.168.1.1', NULL, NULL, N'192.168.1.1', NULL, 0)
 GO
-INSERT [dbo].[User] ([Id], [Name], [Email], [Password], [UserType], [CreatedDate], [CreatedIpAddress], [CreatedUserId], [UpdatedDate], [UpdatedIpAddress], [UpdatedUserId], [IsDeleted]) VALUES (2, N'Joe Simpson', N'user@user.com', '1',  2, CAST(N'2015-01-01 00:00:00.000' AS DateTime), N'192.168.1.1', 1, CAST(N'2015-11-16 16:01:22.910' AS DateTime), N'192.168.1.1', 1, 0)
+INSERT [dbo].[User] ([Id], [Name], [Email], [Password], [UserType], [CreatedDate], [CreatedIpAddress], [CreatedUserId], [UpdatedDate], [UpdatedIpAddress], [UpdatedUserId], [IsDeleted]) VALUES (2, N'Joshua Jinu', N'joshua@user.com', '123',  2, CAST(N'2015-01-01 00:00:00.000' AS DateTime), N'192.168.1.1', 1, CAST(N'2015-11-16 16:01:22.910' AS DateTime), N'192.168.1.1', 1, 0)
 GO
 SET IDENTITY_INSERT [dbo].[User] OFF
 GO
